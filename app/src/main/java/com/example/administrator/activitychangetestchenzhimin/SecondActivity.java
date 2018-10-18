b2580a70611a464fc1214d3d089309e80542129a
chenzhimin1 <1065180490@qq.com>
2018-10-18 16:41:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/activitychangetestchenzhimin/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.example.administrator.activitychangetestchenzhimin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+    private Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
